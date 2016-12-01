a0edd5868714ce90f39d12d61a0081d05f6d1506
Daniel Meixner <daniel.meixner@live.com>
2016-12-01 12:42:12 +0100

    coding skills alog

---
+++ ts16test/BusinessLogic/DevSkillComputer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public static class DevSkillComputer
+    {
+        public static double ComputeDeveloperCodingsSkills(
+                int yearsOfExperience, 
+                int githubRepos, 
+                int stickersOnLaptop, 
+                int shoesize){
+            double factor = 1;
+
+            if (yearsOfExperience<1)
+            {
+                return 1;
+            }
+
+            switch (shoesize)
+            {
+                case 47:
+                    factor += 0.2;
+                    if (yearsOfExperience >10)
+                        factor += 0.3;
+                    if (yearsOfExperience < 3)
+                        factor += 0.2;
+                        if (stickersOnLaptop > yearsOfExperience)
+                            factor += 0.5;
+                    break;
+                case 36:
+                    factor += 0.01 * stickersOnLaptop / yearsOfExperience;
+                    break;
+                default:
+                    break;
+            }
+
+            if (githubRepos > 10)
+                factor += 0.4;
+                if (stickersOnLaptop == 0)
+                    factor -= 0.5;
+
+            return factor * githubRepos + shoesize / yearsOfExperience;
+        }
+    }
+}
---
+++ ts16test/BusinessLogicTests/DevSkillComputerTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace BusinessLogic.Tests
+{
+    [TestClass()]
+    public class DevSkillComputerTests
+    {
+        [TestMethod()]
+        public void ComputeDeveloperCodingsSkillsTest()
+        {
+            double expectedResult = 6;
+            var result = DevSkillComputer.ComputeDeveloperCodingsSkills(7, 0, 2, 46);
+            Debug.WriteLine(result);
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void ComputeDeveloperCodingsSkillsTest870()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(1, 0, 0, 0);
+            Assert.AreEqual<double>(0, d);
+        }
+
+        [TestMethod]
+       public void ComputeDeveloperCodingsSkillsTest929()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(8, 0, 0, 47);
+            Assert.AreEqual<double>(5, d);
+        }
+
+        [TestMethod]
+       public void ComputeDeveloperCodingsSkillsTest662()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(11, 0, 0, 47);
+            Assert.AreEqual<double>(4, d);
+        }
+
+        [TestMethod]
+       public void ComputeDeveloperCodingsSkillsTest371()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(0, 0, 1, 0);
+            Assert.AreEqual<double>(1, d);
+        }
+
+        [TestMethod]
+       public void ComputeDeveloperCodingsSkillsTest700()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(11, 0, 14, 47);
+            Assert.AreEqual<double>(4, d);
+        }
+
+        [TestMethod]
+        
+        public void ComputeDeveloperCodingsSkillsTest281()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(1, 11, 0, 0);
+            Assert.AreEqual<double>(9.8999999999999986, d);
+        }
+
+        [TestMethod]
+        
+        public void ComputeDeveloperCodingsSkillsTest462()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(11, 0, 0, 36);
+            Assert.AreEqual<double>(3, d);
+        }
+
+        [TestMethod]        
+        public void ComputeDeveloperCodingsSkillsTest166()
+        {
+            double d;
+            d =  DevSkillComputer.ComputeDeveloperCodingsSkills(1, 0, 0, 47);
+            Assert.AreEqual<double>(47, d);
+        }
+
+    }
+}
